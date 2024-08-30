1674aedb027343072ec9eeea809bc304e24171f9
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2024-08-30 14:56:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,61 @@
 import streamlit as st
+import pandas as pd
+import openpyxl
+import plotly.express as px
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+title = "Excel Visualizer"
+st.set_page_config(
+    page_title=title,
+    page_icon=":bar_chart:",
+    layout="wide"
 )
+
+st.header(title)
+
+data_file = st.sidebar.file_uploader(
+    "Select Excel file to visualize", 
+    type=["xlsx", "xlsm", "xltx", "xltm"], 
+    accept_multiple_files=False, 
+    label_visibility="visible"
+)
+
+if data_file:
+    st.sidebar.markdown("""---""")
+    wb = openpyxl.load_workbook(data_file)
+
+    ## Select sheet
+    sheet_selector = st.sidebar.selectbox(
+        "Select sheet to visualize",
+        wb.sheetnames,
+        index=None,
+        placeholder="Select sheet..."
+    )
+    if sheet_selector:
+        st.sidebar.markdown("""---""")
+        df = pd.read_excel(data_file,sheet_selector)
+        x_axis_selector = st.sidebar.selectbox("Select time column",
+            df.columns,
+            index=None,
+            placeholder="Select time column..."
+        )
+        if x_axis_selector:
+            y_axis_selector = st.sidebar.multiselect(
+                "Select measures to display",
+                list(filter(lambda x : x != x_axis_selector, df.columns))
+            )
+            if y_axis_selector:
+                fig = px.line(
+                    df, 
+                    x=x_axis_selector, 
+                    y=y_axis_selector
+                )
+                fig.update_layout(
+                    yaxis=dict(rangemode='tozero',fixedrange=True),  # Fix the Y-axis starting at 0 and prevent zooming
+                    showlegend=True,
+                    xaxis=dict(fixedrange=False),  # Allow zooming on X-axis
+                    plot_bgcolor="rgba(0,0,0,0)",
+                    xaxis_title=None,
+                    yaxis_title=None,
+                    legend_title=None
+                )
+                st.plotly_chart(fig, use_container_width=True)
