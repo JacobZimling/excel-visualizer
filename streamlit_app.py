5db36e854f20419fb9a375e9332febc50b8ceebc
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2024-09-23 15:45:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.header(title)
 data_file = st.sidebar.file_uploader(
     "Select Excel file to visualize", 
 #    type=["xlsx", "xlsm", "xltx", "xltm","csv"], 
-    type=["xlsx", "xlsm", "xltx", "xltm"s], 
+    type=["xlsx", "xlsm", "xltx", "xltm"], 
     accept_multiple_files=False, 
     label_visibility="visible"
 )
