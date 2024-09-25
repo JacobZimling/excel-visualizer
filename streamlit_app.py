c72627be85d6907e13d24ade1668d8c41b5bf056
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2024-09-25 14:22:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,104 +5,9 @@ import plotly.express as px
 from openpyxl import Workbook
 import csv
 
-# Function to convert CSV to OpenPyXL workbook
-def csv_to_workbook(csv_file):
-    # Read CSV into pandas DataFrame
-    df = pd.read_csv(csv_file)
-    
-    # Create an OpenPyXL workbook
-    wb = Workbook()
-    ws = wb.active
-    
-    # Write DataFrame to workbook
-    for r_idx, row in df.iterrows():
-        ws.append(list(row))
-
-    return wb
-
-def do_no_run():
-    title = "Excel Visualizer"
-    st.set_page_config(
-        page_title=title,
-        page_icon=":bar_chart:",
-        layout="wide"#,
-    #    primaryColor = "#E694FF",
-    #    backgroundColor = "#00172B",
-    #    secondaryBackgroundColor = "#0083B8",
-    #    textColor = "#FFF",
-    #    font = "sans serif"
-    )
-    
-    st.header(title)
-    
-    data_file = st.sidebar.file_uploader(
-        "Select Excel file to visualize", 
-        type=["xlsx", "xlsm", "xltx", "xltm","csv"], 
-    #    type=["xlsx", "xlsm", "xltx", "xltm"], 
-        accept_multiple_files=False, 
-        label_visibility="visible"
-    )
-    
-    #print(data_file.name)
-    if data_file:
-    #    st.write("Filename: ", data_file.name)
-    #    st.write("Filename: ", type(data_file))
-        if data_file.name.lower().endswith(".csv"):
-            st.write("Process CSV file")
-    #        wb = Workbook()
-    #        ws = wb.active
-    #        with open(data_file, 'r') as f:
-    #            for row in csv.reader(f):
-    #                ws.append(row)
-            wb = csv_to_workbook(data_file)
-        else:
-            wb = openpyxl.load_workbook(data_file)
-            
-    #exit()
-    
-    #if data_file:
-        st.sidebar.markdown("""---""")
-    #    wb = openpyxl.load_workbook(data_file)
-    
-        ## Select sheet
-        sheet_selector = st.sidebar.selectbox(
-            "Select sheet to visualize",
-            wb.sheetnames,
-            index=None,
-            placeholder="Select sheet..."
-        )
-        if sheet_selector:
-            st.sidebar.markdown("""---""")
-            st.write(type(data_file))
-            df = pd.read_excel(data_file,sheet_selector,engine='openpyxl')
-            x_axis_selector = st.sidebar.selectbox("Select time column",
-                df.columns,
-                index=None,
-                placeholder="Select time column..."
-            )
-            if x_axis_selector:
-                y_axis_selector = st.sidebar.multiselect(
-                    "Select measures to display",
-                    list(filter(lambda x : x != x_axis_selector, df.columns))
-                )
-                if y_axis_selector:
-                    fig = px.line(
-                        df, 
-                        x=x_axis_selector, 
-                        y=y_axis_selector
-                    )
-                    fig.update_layout(
-                        yaxis=dict(rangemode='tozero',fixedrange=True),  # Fix the Y-axis starting at 0 and prevent zooming
-                        showlegend=True,
-                        xaxis=dict(fixedrange=False),  # Allow zooming on X-axis
-                        plot_bgcolor="rgba(0,0,0,0)",
-                        xaxis_title=None,
-                        yaxis_title=None,
-                        legend_title=None
-                    )
-                    st.plotly_chart(fig, use_container_width=True)
-
 title = "Excel Visualizer"
+csv_delimiters = (";",",")
+
 st.set_page_config(
     page_title=title,
     page_icon=":bar_chart:",
@@ -121,6 +26,14 @@ data_file = st.sidebar.file_uploader(
     accept_multiple_files=False, 
     label_visibility="visible"
 )
+
+def csv_delimitor_selector(csv_delimiters):
+    csv_delimiter = st.sidebar.selectbox("Select data delimiter",
+        csv_delimiters,
+        index=None,
+        placeholder="Select data delimiter..."
+    )
+    return csv_delimiter
 
 def time_column_selector(df):
     x_axis_selector = st.sidebar.selectbox("Select time column",
